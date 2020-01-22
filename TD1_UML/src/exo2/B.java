709ba3c93d97fbee77900fda1750152ad42fe32b
josua.trebuchon@etu.umontpellier.fr <josua.trebuchon@etu.umontpellier.fr@i12.fdsetu.infra.umontpellier.fr>
2020-01-22 12:56:48 +0100

    Fin paquetage et visibilité

--- TD1_UML/src/exo2/B.java
+++ TD1_UML/src/exo2/B.java
@@ -3,9 +3,15 @@ package exo2;
 /* export CLASSPATH=$HOME/workspace/TD1_UML/src/:.
 	Commande pour relier les classe entre elles
 */
+import exo2_bis.C;
 
 public class B {
-	public void mc(){}
+	public void mc(){
+		C c =new C();
+		System.out.println(c.c1);
+		//System.out.println(c.c2);
+		//System.out.println(c.c3);
+	}
 	public void ma(){
 		A a = new A();
 		System.out.println(a.a1);
---
+++ TD1_UML/src/test/M.java
@@ -0,0 +1,11 @@
+package test;
+
+import exo2.B;
+
+public class M {
+	public static void main(String [] args){
+		B b=new B();
+		b.ma();
+		b.mc();
+	}
+}
